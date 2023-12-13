f340455508eddb934640b2df80b4819416137b75
Gormery Kombo Wanjiru <gormerykw@uia.no>
2023-12-13 05:51:46 +0100

    fd

---
+++ monorepo.sh
@@ -0,0 +1,3 @@
+git subtree add --prefix=apps/htmx-chat https://github.com/valiantlynx/htmx-chat.git main --squash
+git subtree pull --prefix=apps/htmx-chat https://github.com/valiantlynx/htmx-chat.git main --squash
+git subtree push --prefix=apps/htmx-chat https://github.com/valiantlynx/htmx-chat.git main
